cc86f2c0636a024b38c7802d317b035126577c43
Abhishek Mehra <me.abhishek18@gmail.com>
2022-12-31 13:26:46 -0500

    Init - Added pre trained vectors, cross validation

---
+++ disaster_tweet_streamlit_app/disaster_tweet_app.py
@@ -0,0 +1,142 @@
+from sklearn.ensemble import RandomForestClassifier
+import streamlit as st
+import pandas as pd
+import numpy as np
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from xgboost import XGBClassifier
+
+import util
+
+eda = st.container()
+data_prep = st.container()
+machine_learning = st.container()
+metrics = st.container()
+
+RANDOM_STATE = 1
+
+train_df = pd.DataFrame()
+
+
+with eda:
+    st.header("Disaster Tweet Classification- Data Science Project")
+    st.header('Exploratory Data Analysis')
+
+    # importing the dataset
+
+    tweets_df_train = pd.read_csv('../data/train.csv')
+
+    st.write(tweets_df_train.tail(5))
+
+    # most occuring locations
+    # location_button_output = st.button(
+        # 'Click to see the what are the  most occuring locations in the tweets')
+    # if location_button_output:
+    fig = util.location_occurence(tweets_df_train)
+    st.pyplot(fig)
+
+    # # most occuring keywords in tweets
+    # keyword_button__output = st.button(
+    #     'Click to see what are the  most occuring keyword in tweet')
+    # if keyword_button__output:
+    #     fig = util.keyword_occurence(tweets_df_train)
+    #     st.pyplot(fig)
+
+    # # number of words in a tweet
+    # words_button_ouput = st.button(
+    #     'Click to see the number of words distribution in the tweets')
+
+    # # most common words in a tweet
+    # common_words_button_ouput = st.button(
+    #     'Click to see most common words in the tweet')
+
+
+with data_prep:
+    st.header('Data Preparations For Machine Learning')
+    
+    form_dp = st.form(key='dp')
+
+    data_cleaning_ouput = form_dp.selectbox(
+        'Do you want to clean the tweets off html tags, smileys, URLs',('Yes','No'))
+
+    vectoriser_output = form_dp.selectbox(
+            'Which vectoriser do you want to use', ('CountVectoriser', 'TfidVectoriser'))
+
+
+    data_prep_form_submit_button_output = form_dp.form_submit_button(
+        "Submit for data preparations")
+
+    
+    if data_prep_form_submit_button_output:
+        if data_cleaning_ouput == 'Yes': 
+            tweets_df_train = util.clean_tweets(tweets_df_train, "text")
+
+        #filling missing values in keyword and location
+        tweets_df_train = util.fill_na(tweets_df_train)
+
+        #trim unwanted spaces
+        tweets_df_train = util.trim_spaces(tweets_df_train)
+
+        # vectorisation
+        
+
+        if vectoriser_output == 'CountVectoriser':
+            train_df = util.clean_vectorize_using_count_vectorizer(tweets_df_train,'text')
+
+        else:
+            train_df = util.clean_vectorize_using_tfidf_vectorizer(tweets_df_train,'text')
+
+        #getting dummies for location and keyword
+
+        gd_keyword, gd_location = util.make_dummies(tweets_df_train)
+
+        train_df = pd.concat([train_df, gd_keyword, gd_location],axis=1)  #joining vectorised text,location and keyword features
+
+
+    
+        train_df["target"] = tweets_df_train["target"]
+
+        st.write(train_df.head())
+        st.write(train_df.shape)
+        st.write(train_df['target'])
+
+    
+
+
+with machine_learning:
+    st.header('Training a model to classify tweets')
+
+    
+
+    form_ml = st.form(key='ml')
+    # 2. asking user for Model selection: (a) Random Forest
+    model_selection_ouput = form_ml.selectbox(
+        'Which model do you want to select ?', ('Random Forest Classifier','XGBClassifier', ))
+
+    # 3.asking user for Model selection: (a)number of estimators
+    estimators_input = form_ml.slider(
+        'What should be the number of trees?', min_value=100, max_value=600, step=100)
+
+    # # 3 asking user for max depth
+    # max_depth_input = form_ml.slider(
+    #     'What should be the max depth of trees?', min_value=2, max_value=8, step=1)
+
+    # 4 asking user for cv folds
+    # n_folds = form_ml.slider('How many CV folds?',
+                            #  min_value=5, max_value=10, step=1)
+
+    ml_form_submit_button_output = form_ml.form_submit_button(
+        "Submit for training and evaluation")
+
+
+    if ml_form_submit_button_output:
+        if model_selection_ouput == 'Random Forest Classifier':
+            model = RandomForestClassifier(
+                n_estimators=estimators_input, random_state=RANDOM_STATE, n_jobs=-1)
+        else:
+            model = XGBClassifier(n_estimators=estimators_input,
+                                  max_depth=max_depth_input, random_state=RANDOM_STATE, n_jobs=-1)
+
+        # splitting data
+        X_train, X_test, y_train, y_test = util.ttsplit(train_df)
+        result_test_dic = util.training_eval(model, X_train, X_test, y_train, y_test)
+        st.write('the f1 score is:', result_test_dic['f1'])
---
+++ disaster_tweet_streamlit_app/util.py
@@ -0,0 +1,175 @@
+from matplotlib import pyplot as plt
+from regex import R
+from sklearn.metrics import classification_report, f1_score, precision_score, recall_score
+from sklearn.model_selection import train_test_split
+import pandas as pd
+import re
+import string
+from nltk.tokenize import word_tokenize
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+
+RANDOM_STATE = 1
+
+def fill_na(df):
+    df['keyword'].fillna(df['keyword'].mode()[0],inplace=True)   #replacing NaN in keyword with mode values
+
+
+    df['location'].fillna(df['location'].mode()[0],inplace=True)  #replacing NaN in location with mode values
+
+    return df
+
+
+def trim_spaces(df):
+    df['location'] = df['location'].str.strip()
+
+    return df
+
+
+def make_dummies(df):
+    gd = pd.get_dummies(df['keyword'], prefix = ['keyword'])
+
+
+    gd2 = pd.get_dummies(df['location'], prefix = ['location'])
+
+    return gd, gd2
+
+    
+
+
+def clean_vectorize_using_count_vectorizer(df, text_col):
+    """Convert text column to columns of numbers.
+
+    df[text_col] =>>>>>> df [["feat1", "feat2"......]]
+
+    Args:
+        df (_type_): _description_
+        text_col (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    cv = CountVectorizer()  #creating cv ,CountVectorizer object
+    cv_train = cv.fit_transform(df[text_col]) 
+    df = pd.DataFrame(cv_train.todense(),columns = cv.get_feature_names_out())     
+
+    return df
+
+def clean_vectorize_using_tfidf_vectorizer(df, text_col):
+    """Convert text column to columns of numbers.
+
+    df[text_col] =>>>>>> df [["feat1", "feat2"......]]
+
+    Args:
+        df (_type_): _description_
+        text_col (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    tf = TfidfVectorizer(tokenizer=word_tokenize)
+    tf_train = tf.fit_transform(df[text_col])
+    df = pd.DataFrame(tf_train.todense(), columns = tf.get_feature_names_out())
+    return df
+
+
+def remove_url(text):
+    url=re.compile(r'https?://\S+|www\.\S+')
+    return url.sub(r'',text)
+
+def remove_html(text):
+    html=re.compile(r'<.*?>')    
+    return html.sub(r'',text)   #removing html texts
+
+def remove_punct(text):
+    table = str.maketrans('','',string.punctuation)
+    return text.translate(table)
+
+def remove_emoji(text):
+    emoji_pattern = re.compile('['
+                                u"\U0001F600-\U0001F64F"  # emoticons
+                                u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+                                u"\U0001F680-\U0001F6FF"  # transport & map symbols
+                                u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                                u"\U00002702-\U000027B0"
+                                u"\U000024C2-\U0001F251"
+                                ']+',flags=re.UNICODE)
+    return emoji_pattern.sub(r'',text)
+
+def clean_tweets(df, text_col):
+    df[text_col]=df[text_col].apply(lambda x:remove_html(x))   #apply lambda to all values in df series
+    df[text_col]=df[text_col].apply(lambda x:remove_url(x))  #pass a function and apply it to every single value of the series
+    df[text_col]=df[text_col].apply(lambda x:remove_emoji(x))
+    df[text_col]=df[text_col].apply(lambda x: remove_punct(x))
+    return df
+
+def location_occurence(df):
+    top_10 = df['location'].value_counts()[:10]
+    fig = plt.figure(figsize = (10,4))
+    top_10.plot.barh()
+    return fig
+
+    
+
+def keyword_occurence(df):
+    top_10 = df['keyword'].value_counts()[:10]
+    fig = plt.figure(figsize = (10,4))
+    top_10.plot.barh()
+    return fig
+
+
+def words_distribution(df):
+    fig,(ax1,ax2)=plt.subplots(1,2,figsize=(10,5))
+
+    disaster_tweets = df[df['target']==1]['text']
+    tweets = disaster_tweets.str.split()
+    number_of_words = tweets.map(lambda x:len(x))
+    ax1.hist(number_of_words,color='red')
+    ax1.set_title('Disaster tweets')
+    ax1.set_xlabel('Number of words')
+    ax1.set_ylabel('Number of tweets')
+
+    non_disaster_tweets = df[df['target']==0]['text']
+    tweets = non_disaster_tweets.str.split()
+    number_of_words = tweets.map(lambda x: len(x))
+    ax2.hist(number_of_words,color='green')
+    ax2.set_title('Not disaster tweets')
+    fig.suptitle('Words in a tweet')
+
+
+
+def ttsplit(df, label_col_name='target', test_size=0.2):
+    y = df[label_col_name]
+    df = df.drop(label_col_name, axis=1, inplace=False)
+    assert label_col_name not in df.columns
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        df, y, test_size=test_size, shuffle=True, random_state=RANDOM_STATE, stratify=y)
+
+    return X_train, X_test, y_train, y_test
+
+
+def training_eval(model, X_train, X_test, y_train, y_test):
+    model.fit(X_train, y_train)
+    predictions = model.predict(X_test)
+
+
+
+    # dataframe containing just y_test and predictions
+    output_df = pd.DataFrame({'Actual': y_test, 'Predicted': predictions})
+
+    # miss classified data - false positives
+    fp_df = X_test.loc[(output_df['Actual'] == 0) &
+                       (output_df['Predicted'] == 1)]
+
+    # miss classified data - false negatives
+    fn_df = X_test.loc[(output_df['Actual'] == 1) &
+                       (output_df['Predicted'] == 0)]
+
+    output_dic = {'f1': round(f1_score(y_test, predictions), 3),
+                  'precision': round(precision_score(y_test, predictions), 3),
+                  'recall': round(recall_score(y_test, predictions), 3),
+                  'classification': classification_report(y_test, predictions, output_dict=True),
+                  'false_positves': fp_df,
+                  'false_negatives': fn_df}
+
+    return output_dic
